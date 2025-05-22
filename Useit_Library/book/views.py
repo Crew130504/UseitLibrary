32416e95a3d94f14c9bf1cce692c86634c3254b1
Crew130504 <juanpablomayo13@gmail.com>
2025-05-22 13:19:56 -0500

    feat: implement basic views and routes book

--- Useit_Library/book/views.py
+++ Useit_Library/book/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAdminUser
+from book.models import Book
+from book.serializers import BookSerializer
 
-# Create your views here.
+class BookListView(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'id'
+
+class BookCreateView(CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminUser]
+    
+class BookUpdateView(UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminUser]
+    lookup_field = 'id'
+
+class BookDeleteView(DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminUser]
+    lookup_field = 'id'
