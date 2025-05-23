05eb81db5eec4939835e3cc241f8171eef335a1c
Crew130504 <juanpablomayo13@gmail.com>
2025-05-23 09:43:08 -0500

    feat(deploy): create default superuser during deployment

--- build.sh
+++ build.sh
@@ -8,3 +8,7 @@ python manage.py migrate --noinput
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+echo "Creating superuser..."
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin123')" | python manage.py shell
+
