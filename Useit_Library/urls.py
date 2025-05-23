6a73011157b7177dd8d95500cc49c198a85658da
Crew130504 <juanpablomayo13@gmail.com>
2025-05-23 07:07:10 -0500

    Restore Useit_Library folder from development branch

---
+++ Useit_Library/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('api/books/', include('book.urls')),
+    path('api/loans/', include('loan.urls')),
+    path('api/users/', include('user.urls')),
+
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include('book.urls')),
+]
