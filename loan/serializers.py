82b8ceb21b67ee41ea5c0820418b10c0449377fd
Crew130504 <juanpablomayo13@gmail.com>
2025-05-24 00:40:52 -0500

    deploy finalw

--- loan/serializers.py
+++ loan/serializers.py
@@ -1,8 +1,6 @@
 from rest_framework import serializers
 from loan.models import Loan
 from book.models import Book
-from rest_framework.exceptions import ValidationError
-
 
 class LoanSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField(read_only=True)
@@ -20,11 +18,3 @@ class LoanSerializer(serializers.ModelSerializer):
         validated_data.pop('book', None)
         validated_data.pop('user', None)
         return super().update(instance, validated_data)
-    def validate(self, data):
-        user = self.context['request'].user
-        book = data['book']
-
-        if Loan.objects.filter(user=user, book=book, return_date__isnull=True).exists():
-           raise ValidationError("Before borrowing a new copy, return the previous one.")
-        return data
-    
